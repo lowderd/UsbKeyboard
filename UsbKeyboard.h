8e1afc6c60ad97dc069e0a534aed9caa1aa63a46
lowderd <duncan.lowder@gmail.com>
2015-04-10 19:21:08 -0600

    Added more USB HID Codes as constants

--- UsbKeyboard.h
+++ UsbKeyboard.h
@@ -168,7 +168,73 @@ const PROGMEM char usbHidReportDescriptor[35] = { /* USB report descriptor */
 #define KEY_F9              0x42    // Keyboard F9
 #define KEY_F10             0x43    // Keyboard F10
 #define KEY_F11             0x44    // Keyboard F11
+#define KEY_F12             0x45    // Keyboard F12
 
+#define KEY_PRINT_SCREEN    0x46    // Keyboard Print Screen
+#define KEY_SCROLL_LOCK     0x47    // Keyboard Scroll Lock
+#define KEY_PAUSE           0x48    // Keyboard Pause
+#define KEY_INSERT          0x49    // Keyboard Insert
+#define KEY_HOME            0x4A    // Keyboard Home
+#define KEY_PAGE_UP         0x4B    // Keyboard Page Up
+#define KEY_DELETE          0x4C    // Keyboard Delete
+#define KEY_END             0x4D    // Keyboard End
+#define KEY_PAGE_DOWN       0x4E    // Keyboard Page Down
+
+#define KEY_RIGHT_ARROW     0x4F    // Keyboard Right Arrow
+#define KEY_LEFT_ARROW      0x50    // Keyboard Left Arrow
+#define KEY_DOWN_ARROW      0x51    // Keyboard Down Arrow
+#define KEY_UP_ARROW        0x52    // Keyboard Up Arrow
+#define KEY_NUM_LOCK        0x53    // Keyboard Num Lock
+
+#define KEY_NUM_SLASH       0x54    // Keyboard Num Pad /
+#define KEY_NUM_ASTERISK    0x55    // Keyboard Num Pad *
+#define KEY_NUM_DASH        0x56    // Keyboard Num Pad -
+#define KEY_NUM_PLUS        0x57    // Keyboard Num Pad +
+#define KEY_NUM_ENTER       0x58    // Keyboard Num Pad Enter
+
+#define KEY_NUM_1           0x59    // Keyboard Num Pad 1 and End
+#define KEY_NUM_2           0x5A    // Keyboard Num Pad 2 and Down Arrow
+#define KEY_NUM_3           0x5B    // Keyboard Num Pad 3 and Page Down
+#define KEY_NUM_4           0x5C    // Keyboard Num Pad 4 and Left Arrow
+#define KEY_NUM_5           0x5D    // Keyboard Num Pad 5 
+#define KEY_NUM_6           0x5E    // Keyboard Num Pad 6 and Right Arrow
+#define KEY_NUM_7           0x5F    // Keyboard Num Pad 7 and Home
+#define KEY_NUM_8           0x60    // Keyboard Num Pad 8 and Up Arrow
+#define KEY_NUM_9           0x61    // Keyboard Num Pad 9 and Page Up
+#define KEY_NUM_0           0x62    // Keyboard Num Pad 0 and Insert
+#define KEY_NUM_PERIOD      0x63    // Keyboard Num Pad .
+
+#define KEY_APP             0x65    // Keyboard Application
+#define KEY_PWR             0x66    // Keyboard Power
+#define KEY_NUM_PAD_EQUAL   0x67    // Keyboard Num Pad Equal
+
+#define KEY_F13             0x68    // Keyboard F13
+#define KEY_F14             0x69    // Keyboard F14
+#define KEY_F15             0x6A    // Keyboard F15
+#define KEY_F16             0x6B    // Keyboard F16
+#define KEY_F17             0x6C    // Keyboard F17
+#define KEY_F18             0x6D    // Keyboard F18
+#define KEY_F19             0x6E    // Keyboard F19
+#define KEY_F20             0x6F    // Keyboard F20
+#define KEY_F21             0x70    // Keyboard F21
+#define KEY_F22             0x71    // Keyboard F22
+#define KEY_F23             0x72    // Keyboard F23
+#define KEY_F24             0x73    // Keyboard F24
+
+#define KEY_EXECUTE         0x74    // Keyboard Execute
+#define KEY_HELP            0x75    // Keyboard Help
+#define KEY_MENU            0x76    // Keyboard Menu
+#define KEY_SELECT          0x77    // Keyboard Select
+#define KEY_STOP            0x78    // Keyboard Stop
+#define KEY_AGAIN           0x79    // Keyboard Again
+#define KEY_UNDO            0x7A    // Keyboard Undo
+#define KEY_CUT             0x7B    // Keyboard Cut
+#define KEY_COPY            0x7C    // Keyboard Copy
+#define KEY_PASTE           0x7D    // Keyboard Paste
+#define KEY_FIND            0x7E    // Keybaord Find
+#define KEY_MUTE            0x7F    // Keyboard Mute
+#define KEY_VOL_UP          0x80    // Keyboard Volume Up
+#define KEY_VOL_DOWN        0x81    // Keyboard Volume Down
 
 
 class UsbKeyboardDevice {
